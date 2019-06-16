fca1bbdef58a33620f30b4f4fb7093d8490c0664
rohitgowan <rohitgowan@users.noreply.github.com>
2019-06-16 08:07:55 +0530

    a new method added in First.java

--- First.java
+++ First.java
@@ -2,4 +2,7 @@ class First{
 public static void main(String args[]){
  System.out.println("this is my first file in GIT");
 }
+public void newMethod(){
+System.out.println("new method added");
+}
 }
